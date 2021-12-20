ca1cb65792af12dd932184ac634c46b560c4c79f
Kyle Barnes <kylebarnes2003@outlook.com>
2021-12-20 21:30:45 +0000

    fixes and tests

--- Card Game/src/texasHoldEm.py
+++ Card Game/src/texasHoldEm.py
@@ -7,62 +7,37 @@ class TexasHoldEm:
     playingCard = PlayingCard()
     input = ConsoleInput()
     output = ConsoleOutput()
+    deck = playingCard.generate_deck()
+    deck = playingCard.shuffle_cards(deck)
 
     def set_input(self, input_to_set):
         self.input = input_to_set
 
     def deal_cards(self):
-        deck = self.playingCard.generate_deck()
-        deck = self.playingCard.shuffle_cards(deck)
-        hands = self.playingCard.deal_cards(deck, 2, 2)
-        table_cards = self.playingCard.deal_cards(deck, 5, 1)
+        hands = self.playingCard.deal_cards(self.deck, 2, 2)
+        table_cards = self.playingCard.deal_cards(self.deck, 3, 1)
+        table_cards = table_cards[0]
         return hands, table_cards
 
     def play_game(self, hands, table_cards):
         fold = "F"
         print("Your hand: " + str(hands[0]) + "  " + "Computers hand: " + str(
             hands[1]))
+        for i in range(len(table_cards)):
+            print("Card " + str(i + 1) + ": " + table_cards[i])
+
+        while len(table_cards) <= 5:
+            fold = (self.input.get_string("Would you like to (C)all or (F)old? ")).upper()
+            while fold not in ["F", "C"]:
+                fold = (self.input.get_string("Error, enter 'C' for call or 'F' for fold: ")).upper()
+            if fold == "F" or len(table_cards) > 4:
+                break
+            else:
+                table_cards.append(self.deck.pop())
+                print("Card " + str(len(table_cards)) + ": " + table_cards[-1])
+
         print()
-        print("Card " + str(1) + ": " + table_cards[0][0])
-        print("Card " + str(2) + ": " + table_cards[0][1])
-        for i in range(2, len(table_cards[0])):
-            print(
-                "Card " + str(i + 1) + ": " + table_cards[0][i])
-            if i == len(table_cards[0]) - 1:
-                break
-            fold = self.input.get_string("Would you like to (C)all or (F)old? ")
-            while fold not in ["F", "f", "C", "c"]:
-                fold = self.input.get_string("Error, enter 'C' for call or 'F' for fold: ")
-            if fold == "F" or fold == "f":
-                break
-            print()
         return fold
-
-    def determine_winner(self, hands, table_cards):
-        user_hand = hands[0]
-        comp_hand = hands[1]
-
-        win_ranks = {1: "Straight Flush",
-                     2: "Four of a Kind",
-                     3: "Full House",
-                     4: "Flush",
-                     5: "Straight",
-                     6: "Three of a Kind",
-                     7: "Two Pair",
-                     8: "Pair",
-                     9: "Highest Card"}
-
-        same_suit, straight, four_pair, three_pair, pair, number_of_pairs = self.game_rules(user_hand, table_cards)
-
-        comp_win_rank = self.get_win_rank(same_suit, straight, four_pair, three_pair, pair,
-                                          number_of_pairs)
-
-        same_suit, straight, four_pair, three_pair, pair, number_of_pairs = self.game_rules(comp_hand, table_cards)
-
-        user_win_rank = self.get_win_rank(same_suit, straight, four_pair, three_pair, pair,
-                                          number_of_pairs)
-
-        return user_win_rank, comp_win_rank, win_ranks
 
     @staticmethod
     def get_win_rank(same_suit, straight, four_pair, three_pair, pair, number_of_pairs):
@@ -86,12 +61,39 @@ class TexasHoldEm:
             win = 9
         return win
 
+    def determine_winner(self, hands, table_cards):
+        user_hand = hands[0]
+        comp_hand = hands[1]
+
+        win_ranks = {1: "Straight Flush",
+                     2: "Four of a Kind",
+                     3: "Full House",
+                     4: "Flush",
+                     5: "Straight",
+                     6: "Three of a Kind",
+                     7: "Two Pair",
+                     8: "Pair",
+                     9: "Highest Card"}
+
+        same_suit, straight, four_pair, three_pair, pair, number_of_pairs = self.game_rules(user_hand, table_cards)
+
+        user_win_rank = self.get_win_rank(same_suit, straight, four_pair, three_pair, pair,
+                                          number_of_pairs)
+
+        same_suit, straight, four_pair, three_pair, pair, number_of_pairs = self.game_rules(comp_hand, table_cards)
+
+        comp_win_rank = self.get_win_rank(same_suit, straight, four_pair, three_pair, pair,
+                                          number_of_pairs)
+
+        return user_win_rank, comp_win_rank, win_ranks
+
     def game_rules(self, hand, table_cards):
+
         same_suit = self.suit_check(hand, table_cards)
 
-        straight = self.straight_check(hand)
+        straight = self.straight_check(self.get_card_number_list(hand, table_cards))
 
-        four_pair, three_pair, pair, number_of_pairs = self.pair_check(hand)
+        four_pair, three_pair, pair, number_of_pairs = self.pair_check(self.get_card_number_list(hand, table_cards))
 
         return same_suit, straight, four_pair, three_pair, pair, number_of_pairs
 
@@ -100,8 +102,8 @@ class TexasHoldEm:
 
         for x in range(len(hand)):
             number_list.append(hand[x])
-        for x in range(len(table_cards[0])):
-            number_list.append(table_cards[0][x])
+        for x in range(len(table_cards)):
+            number_list.append(table_cards[x])
 
         self.playingCard.convert_faces_to_numbers(number_list)
 
@@ -118,7 +120,7 @@ class TexasHoldEm:
         same_suit = False
         for x in range(len(hand)):
             for i in range(len(table_cards[0])):
-                if hand[x][0:1] == table_cards[0][i][0:1]:
+                if hand[x][0:1] == table_cards[i][0:1]:
                     suit_counter += 1
             if suit_counter == 5:
                 same_suit = True
@@ -164,10 +166,10 @@ class TexasHoldEm:
 
     def display_winner(self, user_win_rank, comp_win_rank, win_ranks):
         print(user_win_rank, comp_win_rank)
-        if user_win_rank > comp_win_rank:
+        if user_win_rank < comp_win_rank:
             self.output.display("Congratulations, you have won with a " + win_ranks.get(
                 user_win_rank))
-        elif user_win_rank < comp_win_rank:
+        elif user_win_rank > comp_win_rank:
             self.output.display("Sorry, the computer won with a " + win_ranks.get(comp_win_rank))
         else:
             self.output.display("Draw! You both have a " + win_ranks.get(user_win_rank))
--- Card Game/test/texasHoldEmTest.py
+++ Card Game/test/texasHoldEmTest.py
@@ -1,22 +1,38 @@
 import unittest
 from texasHoldEm import TexasHoldEm
 from PlayingCard import PlayingCard
+from ConsoleInput import ConsoleInput
 
 
 class TexasHoldEmTest(unittest.TestCase):
     playingCard = PlayingCard()
+    texas = TexasHoldEm()
+    input = ConsoleInput()
+    hands, table_cards = texas.deal_cards()
 
     def test_hands(self):
-        hands, table_cards = TexasHoldEm.deal_cards(self)
-        self.assertEqual(2, len(hands))
+        self.assertEqual(2, len(self.hands))
 
     def test_initial_table_card(self):
-        hands, table_cards = TexasHoldEm.deal_cards(self)
-        self.assertEqual(1, len(table_cards))
+        self.assertEqual(3, len(self.table_cards))
 
     def test_play_game(self):
-        hands, table_cards = TexasHoldEm.deal_cards(self)
-        self.assertEqual("F", TexasHoldEm.play_game(self, hands, table_cards))
+        fold = self.texas.play_game(self.hands, self.table_cards)
+        self.assertTrue(fold in ["F", "C"])
+
+    def test_get_win_rank(self):
+        self.assertEqual(int(self.input.get_string("leave blank for false or enter any character for true\nwin rank: ")),
+                         self.texas.get_win_rank(
+                             (bool(self.input.get_string("suit: "))),
+                             (bool(self.input.get_string("straight: "))),
+                             (bool(self.input.get_string("four pair: "))),
+                             (bool(self.input.get_string("three pair: "))),
+                             (bool(self.input.get_string("pair: "))),
+                             (int(self.input.get_string("no of pairs: "))),
+                         ))
+
+    def test_determine_winner(self):
+
 
 
 if __name__ == '__main__':
